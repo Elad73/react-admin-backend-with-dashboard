97aefad975a9b051d3d6d65045b39c8ebb703272
Elad73 <elad.ron.g@gmail.com>
2023-02-26 16:37:05 +0200

    adding Navbar with left side

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,10 @@ import { CssBaseline, ThemeProvider } from "@mui/material";
 import { createTheme } from "@mui/material";
 import { useMemo } from "react";
 import { useSelector } from "react-redux";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import { themeSettings } from "theme";
+import Layout from 'scenes/layout';
+import Dashboard from 'scenes/dashboard';
 
 function App() {
   const mode = useSelector((state) => state.global.mode);
@@ -10,10 +13,18 @@ function App() {
 
   return (
     <div className="app">
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        Backend Client application
-      </ThemeProvider>
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          Backend Client application
+          <Routes>
+            <Route element={<Layout />}>
+              <Route path='/' element={<Navigate to='/dashboard' repleace />} />
+              <Route path='/dashboard' element={<Dashboard />} />
+            </Route>
+          </Routes>
+        </ThemeProvider>
+      </BrowserRouter>
     </div>
   );
 }
